81665893f788bd46224527d385ffb4aa222b02a0
nswam <nswam@LAPTOP-T6QDBDPO>
2026-04-24 12:11:43 +0530

    Added New Line

--- GitHub_Project/src/com/java/FirstProgram.java
+++ GitHub_Project/src/com/java/FirstProgram.java
@@ -3,5 +3,6 @@ package com.java;
 public class FirstProgram {
   void print() {
 	  System.out.println("Welcome to my Github Session 2");
+	  System.out.println("Hello guys,U want biriyani");
   }
 }
